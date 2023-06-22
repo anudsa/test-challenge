c3af74b00a0327c8602b342ce85ec85d79082196
anudsa <anwardelgadosilva@gmail.com>
2023-06-21 23:58:12 -0600

    Se añade el test de users y se terminan los ejercicios

--- tests/users.test.js
+++ tests/users.test.js
@@ -4,6 +4,21 @@ const {getUser, getUsers} = require('../users');
 
 
 // Make a test to verify if the method getUser(id) works correctly
+
+test('checks if the correct user is retrieved',()=>{
+    const data = 
+    {
+        id: 3,
+        username: "goosemotionless",
+        email: "dotmeheart@outlook.com",
+        password: "12345abcde"
+    };
+
+    expect(getUser(3)).toEqual(data);
+});
+
 // Verify any data of the resulting object you want
 
-// READ documentation
+test('testing if getUser(id) works by giving id=3, it should ouput a 3 id.',()=>{
+    expect(getUser(3).id).toBe(3);
+});
