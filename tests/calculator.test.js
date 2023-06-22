aea453ea449daf0aa337ee18de7f9b0b27615207
anudsa <anwardelgadosilva@gmail.com>
2023-06-21 22:30:34 -0600

    test for calc written properly, failed due to error in power function

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -3,22 +3,22 @@ const { default: expect } = require('expect');
 const calculator = require('../calculator');
 
 test('adds a + b', () => {
-    //
+    expect(calculator.sum(10,10)).toBe(20);
 })
 
 test('substracts a - b', () => {
-   //
+    expect(calculator.substract(10,5)).toBe(5);
 })
 
 test('multiplies a * b', () => {
-    //
+    expect(calculator.multiply(2,3)).toBe(6);
 })
 
 test('divides a / b', () => {
-    // Consider decimals
+    expect(calculator.divide(10,4)).toBe(2.5);
 })
 
 test('Raises to power a ^ b', () => {
-    //
+    expect(calculator.power(2,3)).toBe(8);
 })
 
