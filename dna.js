8a1d32c16d5b39d41dc08634e7aada88c4cdc1fa
anudsa <anwardelgadosilva@gmail.com>
2023-06-21 23:14:37 -0600

    Se finaliza dna test

---
+++ dna.js
@@ -0,0 +1,13 @@
+
+function canonDNA(DNA) {
+    let stringDNA = '';
+    const nucleoBases = ['C','T','A','G'];
+    for (let i = 0; i < DNA.length; i++) {
+        if (nucleoBases.indexOf(DNA[i]) !== -1) {
+        stringDNA += DNA[i];
+    }
+    }
+    return stringDNA;
+}
+module.exports = canonDNA;
+// ejemplo: console.log(canonDNA("C T A asdfG"));
--- tests/dna.test.js
+++ tests/dna.test.js
@@ -1,1 +1,8 @@
 const canonDNA=require('../dna')
+
+test('Filtra letras no canónicas',()=>{
+    expect(canonDNA("C T A asdfG")).toBe("CTAG");
+});
+test('Manda cadena vacía',()=>{
+    expect(canonDNA("")).toBe("");
+});
